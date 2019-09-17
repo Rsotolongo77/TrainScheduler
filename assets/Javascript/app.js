6a13fb6551074a8e7b31edef6ee04a18e72e5137
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-17 19:45:48 -0400

    firebase configured.

---
+++ assets/Javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function()  {
+    var firebaseConfig = {
+        apiKey: "AIzaSyDM3dvO4YG5KWJx0MPgdvXpmmYs1k4sCzI",
+        authDomain: "traintime-8bd04.firebaseapp.com",
+        databaseURL: "https://traintime-8bd04.firebaseio.com",
+        projectId: "traintime-8bd04",
+        storageBucket: "",
+        messagingSenderId: "1026634328097",
+        appId: "1:1026634328097:web:28f5ee7b47b3dee1e29b60"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
